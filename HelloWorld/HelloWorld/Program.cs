b2489ec2f234508a4231b5dedcf07a2e66ee7f3c
AnnelieP1 <annelie.pettersson@yh.nackademin.se>
2023-09-04 10:07:02 +0200

    testar

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string name = "Annelie";
+Console.WriteLine(name);
+
+string firstName = "Annelie " ;
+string lastName = "Pettersson";
+string fullName = firstName + lastName; 
+Console.WriteLine(fullName);
+
+string placeholder = ($"Hej jag heter {firstName}{lastName}.");
+Console.WriteLine(placeholder);
+
---
+++ tränar_på_int/tränar_på_int/Program.cs
@@ -0,0 +1,9 @@
+﻿string firstName = "Annelie ";
+string lastName = "Pettersson";
+string fullName = firstName + lastName;
+
+string placeholder = ($"Jag heter {fullName}.");
+Console.WriteLine(placeholder);
+int age = 44; 
+string output = ($"Jag är " + age + " år gammal.");
+Console.WriteLine(output);
