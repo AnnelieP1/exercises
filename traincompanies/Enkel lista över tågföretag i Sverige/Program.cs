5f42c154ff0b16f797414da78f25893be96dda69
AnnelieP1 <annelie.pettersson@yh.nackademin.se>
2023-09-10 20:32:10 +0200

    Ny övning

---
+++ traincompanies/Enkel lista över tågföretag i Sverige/Program.cs
@@ -0,0 +1,17 @@
+﻿var traincompanies = new List<string> { "<train>", "SJ", "MTRX", "Flixtrain" };
+
+foreach (var train in traincompanies)
+
+
+{
+    Console.WriteLine(train);
+}
+
+Console.WriteLine();
+
+traincompanies.Add("Mälartågen");
+foreach (var train in traincompanies)
+    Console.WriteLine(train);
+
+Console.WriteLine($"Jag har lagt till {traincompanies[4]} till listan.");
+
